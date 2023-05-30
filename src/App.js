26493e29294fa202a792347ce11776bc50d5c77d
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2023-05-31 00:19:12 +0530

    Learnign React

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import User from './User';
 
-
-function App() {
-  const [status,setStatus]=useState(false);
-
-
-  return (
-    <div className='App'>
-{
-status?<h1>Bhakti</h1>:null
-}
-   <button onClick={()=>setStatus(true)}>Show</button>
-   <button onClick={()=>setStatus(false)}>Hide</button>
+function App(){
+  function getData(){
+    alert("Hello");
+  }
+  return(
+    <div className="App">
+      <User data={getData}/>
     </div>
-  );
+  )
 }
-
 export default App;
--- src/User.js
+++ src/User.js
@@ -1,6 +1,9 @@
-function  User(){
+function  User(props){
     return(
+        <div>
         <h1>User Component</h1>
+        <button onClick={props.data}>Call data function</button>
+        </div>
     )
 }
 export default User;
